bf8dd426e45ee67ecf2d6d725be187d7dbd463e7
WALMIR PEREIRA DE LIMA <79805351+walliots@users.noreply.github.com>
2023-05-09 19:27:38 -0300

    total

--- src/main/java/projetoVendas/VendasApplication.java
+++ src/main/java/projetoVendas/VendasApplication.java
@@ -1,17 +1,11 @@
 package projetoVendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import projetoVendas.domain.entity.Cliente;
-import projetoVendas.domain.repository.Clientes;
+
 
 @SpringBootApplication
+
 public class VendasApplication {
 //    @Bean
 //    public CommandLineRunner commandLineRunner (@Autowired Clientes clientes){
--- src/main/java/projetoVendas/rest/controller/UsuarioController.java
+++ src/main/java/projetoVendas/rest/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package projetoVendas.rest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import projetoVendas.domain.entity.Usuario;
+import projetoVendas.domain.repository.UsuarioRepository;
 import projetoVendas.exception.SenhaInvalidaException;
 import projetoVendas.rest.dto.CredenciaisDTO;
 import projetoVendas.rest.dto.TokenDTO;
@@ -15,9 +17,11 @@ import projetoVendas.security.jwt.JwtService;
 import projetoVendas.service.impl.UsuarioServiceImpl;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class UsuarioController {
 
@@ -26,6 +30,9 @@ public class UsuarioController {
     private final PasswordEncoder passwordEncoder;
 
     private final JwtService jwtService;
+
+    @Autowired
+    private final UsuarioRepository usuarioRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -47,5 +54,10 @@ public class UsuarioController {
         } catch (UsernameNotFoundException | SenhaInvalidaException e ){
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
         }
+    }
+    @GetMapping("/listUser")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Usuario> listUser(){
+        return usuarioRepository.findAll();
     }
 }
